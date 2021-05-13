4c4dc650cbf8c3b24677d895fcce197587b5e559
Michael <mfw6mw@virginia.edu>
2021-05-13 12:39:15 -0400

    added new action to trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -14,4 +14,29 @@ class TripsController < ApplicationController
         end
     end
 
+    def new
+        if params[:scout_id]
+           find_scout
+           @trip = @scout.trips.build
+        else
+           @trip = Trip.new 
+        end
+    end
+    def create
+        if params[:scout_id]
+            find_scout
+            @trip = @scout.trips.build(trip_with_user_params)
+        else
+            @trip = Trip.new(trip_params)
+        end
+        if @trip.save
+           if @scout
+                    redirect_to scout_trip_path(@scout, @trip)
+                else
+                    redirect_to @trip 
+                end
+        else
+            render :new
+        end
+
 end
