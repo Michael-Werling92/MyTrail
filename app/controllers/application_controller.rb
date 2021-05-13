b99fae29db1060a81344cf6d4534ba8cb664ee87
Michael <mfw6mw@virginia.edu>
2021-05-13 12:04:11 -0400

    corrected helper methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
     
-    helper_method :logged_in?, :current_user
+    helper_method :logged_in?, :current_scout
 
 
     def current_scout
-        @scout = Scout.find_by_id(session[:user_id])
+        @scout = Scout.find_by_id(session[:scout_id])
     end
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
 class SessionsController < ApplicationController
+    def new
+    end
+    def create
+        @scout = Scout.find_by(username: params[:username])
+        if @scout && @scout.authenticate(params[:password])
+            session[:scout_id] = @scout.id
+            redirect_to scout_path(@scout)
+        else
+            redirect_to login_path
+        end
+    end
+
+    def login
+    end
+    def destroy
+        session.clear
+        redirect_to login_path   
+    end
+
 end
