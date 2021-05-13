18115ee700cc556292b966877bd06b7366cc24cc
Michael <mfw6mw@virginia.edu>
2021-05-12 22:30:55 -0400

    added Scout migration

--- db/migrate/20210513015709_create_scouts.rb
+++ db/migrate/20210513015709_create_scouts.rb
@@ -1,6 +1,11 @@
 class CreateScouts < ActiveRecord::Migration[6.1]
   def change
     create_table :scouts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :rank
+      t.string :username
+      t.string :password_digest
 
       t.timestamps
     end
