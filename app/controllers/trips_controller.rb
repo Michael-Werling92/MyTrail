fc37fedfd14625c47376d15a6cac7b639a765a21
Michael <mfw6mw@virginia.edu>
2021-05-13 12:59:43 -0400

    added private methods to trips cont

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -22,5 +22,40 @@ class TripsController < ApplicationController
            @trip = Trip.new 
         end
     end
+    def create
+        if params[:scout_id]
+            find_scout
+            @trip = @scout.trips.build(trip_with_scout_params)
+        else
+            @trip = Trip.new(trip_params)
+        end
+            if @trip.save
+                if @scout
+                    redirect_to scout_trip_path(@scout, @trip)
+                else
+                    redirect_to @trip
+                end
+            else
+            render :new
+            end
+    end 
 
 end
+
+private 
+
+
+  def find_trip
+      @trip = Trip.find_by_id(params[:id])
+  end
+     def trip_params
+         params.require(:trip).permit(:location, :date)
+     end 
+
+
+  def find_scout
+      @scout = Scout.find_by_id(params[:scout_id])
+  end
+     def trip_with_scout_params
+         params.require(:trip).permit(:location, :date, :scout_id)
+     end 
