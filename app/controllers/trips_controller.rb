b4639fc5fc764a55795f1136545b0f221ad47f76
Michael <mfw6mw@virginia.edu>
2021-05-13 12:08:32 -0400

    added index and show actions to controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,17 @@
 class TripsController < ApplicationController
+
+    def index
+        if params[:scout_id]
+           find_scout
+           @trips = @scout.trips
+         else
+           @trips = Scouts.all
+        end
+    end
+    def show
+        if params[:scout_id] 
+            find_scout
+        end
+    end
+
 end
