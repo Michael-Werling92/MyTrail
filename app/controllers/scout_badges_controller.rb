ccfde1640d6fe023b56d794c928b5dfd50dce639
Michael <mfw6mw@virginia.edu>
2021-05-14 10:58:23 -0400

    added to scout_badge controller

--- app/controllers/scout_badges_controller.rb
+++ app/controllers/scout_badges_controller.rb
@@ -1,6 +1,25 @@
 class ScoutBadgesController < ApplicationController
-    def index
-        @scout_badges = ScoutBadge.all
+    def new
+        @scout_badge = ScoutBadge.new
     end
+    
+
+
+
+
+
+
+
+
+
+    private 
+
+
+    def find_scout_badge
+        @scout_badge = ScoutBadge.find_by_id(params[:id])
+    end
+    def scout_badge_params
+        params.require(:scout_badge).permit(:fun, :date, :user_id, :hobby_id)
+    end 
 
 end
